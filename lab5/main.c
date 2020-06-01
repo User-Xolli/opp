4bcba575cc699582550c2803649783da69cb0556
xolli <xolli@pm.me>
2020-06-01 23:04:19 +0700

    add comments

--- lab5/main.c
+++ lab5/main.c
@@ -1,25 +1,26 @@
-#include <pthread.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <mpi.h>
+#include <pthread.h> // for pthread function
+#include <stdbool.h> // for true, false and bool
+#include <stdlib.h>  // for malloc, realloc and abs
+#include <stdio.h>   // for perror, puts and printf
+#include <math.h>    // for sqrt
+#include <mpi.h>     // for MPI function
 
+// configs
 #define DEFAULT_SIZE_BUFFER_TASKS (30)
-#define COUNT_ITERATION (50)
-#define CRITICAL_TASKS (50)
+#define COUNT_ITERATION           (50)
+#define CRITICAL_TASKS            (5)
 
+// MPI tags
 #define MPI_TAG_REQUEST (0)
-#define MPI_TAG_ANSWER (1)
-#define MPI_TAG_DATA (2)
-#define MPI_TAG_RESULT (3)
+#define MPI_TAG_ANSWER  (1)
+#define MPI_TAG_DATA    (2)
+#define MPI_TAG_RESULT  (3)
 
-#define TASK_REQUEST (1)
-#define NO_TASKS (2)
-#define HAVE_TASKS (3)
+// My protocol
+#define TASK_REQUEST     (1)
+#define NO_TASKS         (2)
+#define HAVE_TASKS       (3)
 #define END_WORK_REQUEST (4)
-
-int tmp[2];
 
 struct Vector {
     long* data;
@@ -55,13 +56,13 @@ int add_elem(Vector* vector, size_t new_elem) {
 }
 
 pthread_mutex_t task_mutex;
-pthread_cond_t tasks_change;
+pthread_cond_t change_tasks;
 pthread_cond_t few_tasks;
 pthread_cond_t search_tasks;
 bool have_tasks;
 Vector tasks;
 
-// use with lock task_mutex
+// use only with task_mutex lock
 long get_count_tasks() {
     long sum = 0;
     for (size_t i = 0; i < tasks.size; ++i) {
@@ -72,14 +73,10 @@ long get_count_tasks() {
 
 void* controller (void* args) {
     pthread_mutex_lock(&task_mutex);
-    //printf("(%d) lock controller\n", tmp[0]);
     while (true) {
-        //printf("(%d) unlock and wait controller\n", tmp[0]);
-        pthread_cond_wait(&tasks_change, &task_mutex);
-        //printf("(%d) wakeup and lock controller\n", tmp[0]);
+        pthread_cond_wait(&change_tasks, &task_mutex);
         long sum_tasks = get_count_tasks();
         if (sum_tasks == -1) {
-            //printf("(%d) unlock controller [end]\n", tmp[0]);
             break;
         }
         else if (sum_tasks <= CRITICAL_TASKS) {
@@ -93,11 +90,8 @@ void* loader (void* args) {
     int rank = ((int*)args)[0];
     int size = ((int*)args)[1];
     pthread_mutex_lock(&task_mutex);
-    //printf("(%d) lock loader\n", tmp[0]);
     while (true) {
-        //printf("(%d) unlock and wait loader\n", tmp[0]);
         pthread_cond_wait(&few_tasks, &task_mutex);
-        //printf("(%d) wakeup and lock loader\n", tmp[0]);
         if (tasks.data[0] == -1) {
             break;
         }
@@ -105,67 +99,53 @@ void* loader (void* args) {
             continue;
         }
         pthread_mutex_unlock(&task_mutex);
-        //printf("(%d) wakeup and unlock loader\n", tmp[0]);
         for (int proc = 0; proc < size; ++proc) {
             if (proc == rank && proc == size - 1) {
                 pthread_mutex_lock(&task_mutex);
-                //printf("(%d) lock loader [I am last -> no tasks]\n", tmp[0]);
                 have_tasks = false;
-                //printf("(%d) signal about no tasks\n", tmp[0]);
                 pthread_cond_signal(&search_tasks);
             }
             if (proc == rank) {
                 continue;
             }
             int request = TASK_REQUEST;
-            //printf("(%d) MPI_Send request begin\n", tmp[0]);
             MPI_Send(&request, 1, MPI_INT, proc, MPI_TAG_REQUEST, MPI_COMM_WORLD);
-            //printf("(%d) MPI_Send request end\n", tmp[0]);
             int answer;
-            //printf("(%d) MPI_Recv answer begin\n", tmp[0]);
             MPI_Recv(&answer, 1, MPI_INT, proc, MPI_TAG_ANSWER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            //printf("(%d) MPI_Recv answer end\n", tmp[0]);
             if (answer == HAVE_TASKS) {
                 long new_task;
                 MPI_Recv(&new_task, 1, MPI_LONG, proc, MPI_TAG_DATA, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 pthread_mutex_lock(&task_mutex);
-                //printf("(%d) lock loader [have tasks]\n", tmp[0]);
                 if (add_elem(&tasks, new_task) != 0) {
                     perror("Error allocate memory");
                     pthread_mutex_unlock(&task_mutex);
-                    //printf("(%d) unlock loader [error]\n", tmp[0]);
                     return NULL;
                 }
-                pthread_cond_signal(&tasks_change);
+                pthread_cond_signal(&change_tasks);
                 pthread_cond_signal(&search_tasks);
                 break;
             } else if (proc == size - 1) {
                 pthread_mutex_lock(&task_mutex);
-                //printf("(%d) lock loader [Go to last proc and no tasks]\n", tmp[0]);
                 have_tasks = false;
-                //printf("(%d) signal about no tasks\n", tmp[0]);
                 pthread_cond_signal(&search_tasks);
             }
         }
     }
     pthread_mutex_unlock(&task_mutex);
-    //printf("(%d) unlock loader [end]\n", tmp[0]);
     return NULL;
 }
 
-// use with lock task_mutex
+// use only with task_mutex lock
 long take_task() {
     if (tasks.size <= 0) {
         return -1;
     }
     long task = tasks.data[--tasks.size];
-    pthread_cond_signal(&tasks_change);
+    pthread_cond_signal(&change_tasks);
     return task;
 }
 
 void* unloader (void* args) {
-    int rank = ((int*)args)[0];
-    int size = ((int*)args)[1];
     MPI_Status status;
     while (true) {
         int request;
@@ -174,30 +154,20 @@ void* unloader (void* args) {
             break;
         }
         pthread_mutex_lock(&task_mutex);
-        //printf("(%d) lock unloader\n", tmp[0]);
         long count_tasks = get_count_tasks();
         if (count_tasks > CRITICAL_TASKS) {
             long last_task = take_task();
             pthread_mutex_unlock(&task_mutex);
-            //printf("(%d) unlock unloader [have]\n", tmp[0]);
             int send_msg = HAVE_TASKS;
-            //printf("(%d) start MPI_Send unloader [have]\n", tmp[0]);
             MPI_Send(&send_msg, 1, MPI_INT, status.MPI_SOURCE, MPI_TAG_ANSWER, MPI_COMM_WORLD);
-            //printf("(%d) end MPI_Send unloader [have]\n", tmp[0]);
-            //printf("(%d) start MPI_Send unloader [data]\n", tmp[0]);
             MPI_Send(&last_task, 1, MPI_LONG, status.MPI_SOURCE, MPI_TAG_DATA, MPI_COMM_WORLD);
-            //printf("(%d) end MPI_Send unloader [data]\n", tmp[0]);
         } else if (tasks.data[0] == -1) {
             pthread_mutex_unlock(&task_mutex);
-            //printf("(%d) unlock unloader [end]\n", tmp[0]);
             break;
         } else {
             pthread_mutex_unlock(&task_mutex);
-            //printf("(%d) unlock unloader [no have]\n", tmp[0]);
             int send_msg = NO_TASKS;
-            //printf("(%d) start MPI_Send unloader [no have]\n", tmp[0]);
             MPI_Send(&send_msg, 1, MPI_INT, status.MPI_SOURCE, MPI_TAG_ANSWER, MPI_COMM_WORLD);
-            //printf("(%d) end MPI_Send unloader\n", tmp[0]);
         }
     }
     return NULL;
@@ -211,8 +181,8 @@ long double some_work(long task) {
     return result;
 }
 
-// use with lock task_mutex
-void new_iter(int rank, int size, int iter_count) {
+// use only with task_mutex lock
+void init_new_iter(int rank, int size, int iter_count) {
     have_tasks = true;
     for (int i = 0; i < DEFAULT_SIZE_BUFFER_TASKS; ++i) {
         tasks.data[i] = abs(rank + 1 - (iter_count % size)) * 10;
@@ -220,27 +190,23 @@ void new_iter(int rank, int size, int iter_count) {
     tasks.size = DEFAULT_SIZE_BUFFER_TASKS;
 }
 
-// use with lock task_mutex
+// use only with task_mutex lock
 void end_work(int rank) {
     have_tasks = false;
     tasks.data[0] = -1;
     tasks.size = 1;
-    //printf("(%d) signal tasks_change\n", tmp[0]);
-    pthread_cond_signal(&tasks_change);
-    //printf("(%d) signal few_tasks\n", tmp[0]);
+    pthread_cond_signal(&change_tasks);
     pthread_cond_signal(&few_tasks);
     int end_work_request = END_WORK_REQUEST;
     MPI_Send(&end_work_request, 1, MPI_INT, rank, MPI_TAG_REQUEST, MPI_COMM_WORLD);
 }
 
-// use with lock task_mutex
-bool search_task(void) {
+// use only with task_mutex lock
+bool try_search_task(void) {
     if (!have_tasks) {
         return false;
     } else {
-        //printf("(%d) unlock and wait search_task\n", tmp[0]);
         pthread_cond_wait(&search_tasks, &task_mutex);
-        //printf("(%d) wakeup and lock search_task\n", tmp[0]);
     }
     bool ret = have_tasks;
     return ret;
@@ -256,14 +222,12 @@ int main(int argc, char **argv) {
     int info[2];
     MPI_Comm_rank(MPI_COMM_WORLD, info);
     MPI_Comm_size(MPI_COMM_WORLD, info + 1);
-    tmp[0] = info[0];
-    tmp[1] = info[1];
     int ret = 0;
     if (pthread_mutex_init(&task_mutex, NULL) != 0) {
         perror("Error init mutex");
         return 1;
     }
-    if (pthread_cond_init(&tasks_change, NULL) != 0 ||
+    if (pthread_cond_init(&change_tasks, NULL) != 0 ||
         pthread_cond_init(&few_tasks, NULL) != 0 ||
         pthread_cond_init(&search_tasks, NULL) != 0) {
         perror("Error init condition");
@@ -294,30 +258,26 @@ int main(int argc, char **argv) {
         goto exit;
     }
     double time_1 = MPI_Wtime();
-    //printf("(%d) start main cycle\n", tmp[0]);
     long double result = 0.;
+    // main cycle
     for (int it = 0; it < COUNT_ITERATION; ++it) {
-        //printf("(%d) start iteration\n", tmp[0]);
         pthread_mutex_lock(&task_mutex);
-        new_iter(info[0], info[1], it);
+        init_new_iter(info[0], info[1], it);
         long cur_task = take_task();
         pthread_mutex_unlock(&task_mutex);
+        // perform one iteration
         while (cur_task != -1) {
-            //printf("(%d) start new task\n", tmp[0]);
             result += some_work(cur_task);
             pthread_mutex_lock(&task_mutex);
             cur_task = take_task();
             if (cur_task == -1) {
-                //printf("(%d) no task, have_tasks is %d\n", tmp[0], have_tasks);
-                if (!search_task()) {
-                    //printf("(%d) have_tasks is false, break cycle", tmp[0]);
+                if (!try_search_task()) {
                     pthread_mutex_unlock(&task_mutex);
                     break;
                 } else {
                     cur_task = take_task();
                 }
             }
-            //printf("(%d) point 4\n", tmp[0]);
             pthread_mutex_unlock(&task_mutex);
         }
         MPI_Barrier(MPI_COMM_WORLD);
@@ -333,12 +293,10 @@ int main(int argc, char **argv) {
     } else {
         MPI_Send(&result, 1, MPI_LONG_DOUBLE, 0, MPI_TAG_RESULT, MPI_COMM_WORLD);
     }
-    //printf("(%d) end main cycle\n", tmp[0]);
     pthread_mutex_lock(&task_mutex);
     end_work(info[0]);
     pthread_mutex_unlock(&task_mutex);
     pthread_attr_destroy(&attrs);
-    //printf("(%d) start join threads\n", tmp[0]);
     if (pthread_join(task_loader, NULL) != 0) {
         perror("Cannot join threads");
         ret = 1;
@@ -351,19 +309,12 @@ int main(int argc, char **argv) {
         perror("Cannot cancel thread");
         ret = 1;
     }
-    //printf("(%d) end join threads\n", tmp[0]);
     exit:
     pthread_mutex_destroy(&task_mutex);
-    //printf("(%d) mutex destroy\n", tmp[0]);
     pthread_cond_destroy(&few_tasks);
-    //printf("(%d) few_tasks destroy\n", tmp[0]);
     pthread_cond_destroy(&search_tasks);
-    //printf("(%d) search_tasks destroy\n", tmp[0]);
-    pthread_cond_destroy(&tasks_change);
-    //printf("(%d) task_change destroy\n", tmp[0]);
+    pthread_cond_destroy(&change_tasks);
     delete_vector(tasks);
-    //printf("(%d) delete tasks\n", tmp[0]);
     MPI_Finalize();
-    //printf("(%d) MPI_Finalize done\n", tmp[0]);
     return ret;
 }
